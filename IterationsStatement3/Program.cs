f4baaac6211dd16859a26b550ae742f6fc292b93
macuser301 <macuser30@yahoo.com>
2023-03-27 17:37:42 -0500

    first commit

---
+++ IterationsStatement3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace IterationsStatement3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            for (int i = 0; i < 15; i++)
+            {
+                Console.WriteLine(i);
+                
+            }
+
+
+
+        }
+    }
+}
